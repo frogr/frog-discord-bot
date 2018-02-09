5230c22e2b2bc152d0583ed1f5c7bb130faa9805
Austin F <austindanielfrench@gmail.com>
2018-02-08 19:45:58 -0800

    made bulkdelete for a purge command work!

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ client.on('ready', () => {
   );
 });
 
-client.on('message', msg => {
+client.on('message', async msg => {
   if (msg.content.startsWith(prefix)) {
     const args = msg.content.slice(prefix.length).split(/ +/);
     const command = args.shift().toLowerCase();
@@ -21,13 +21,32 @@ client.on('message', msg => {
     console.log(`args: ${args}`);
 
     if (command === 'delete') {
-      msg
-        .delete()
-        .then(msg =>
-          console.log(`deleted "${msg.content}" from ${msg.author}`)
+      const deleteCount = args;
+      if (!deleteCount || deleteCount < 2 || deleteCount > 100)
+        return msg.reply(
+          `command \`${command}\` with argument \`${args}\` is invalid. choose a number between 2 and 10`
         );
+      msg.channel
+        .fetchMessages({ limit: deleteCount })
+        .then(msgs => msg.channel.bulkDelete(msgs));
     }
   }
 });
 
 client.login(token);
+
+// comment hell
+// msg
+//   .delete()
+//   .then(msg =>
+//     console.log(
+//       `deleted "${msg.content}" from ${msg.author} at ${msg.createdAt}. `
+//     )
+//   );
+// if (!deleteCount || deleteCount < 2 || deleteCount > 100)
+//   return msg.reply(
+//     'Please provide a number between 2 and 100 for the number of messages to delete'
+//   );
+// const fetched = await msg.channel.fetchMessages({
+//   count: deleteCount
+// });
