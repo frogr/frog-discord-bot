fa3aa43e89bed4f7f71f5bdf5eb43cbaa638e1b8
Austin F <austindanielfrench@gmail.com>
2018-02-08 21:15:18 -0800

    added simple logging features for members joining and leaving and for other commands

--- app.js
+++ app.js
@@ -13,13 +13,46 @@ client.on('ready', () => {
   );
 });
 
+client.on('guildMemberAdd', member => {
+  const memberLogChannel = member.guild.channels.find('name', 'member-logs');
+  memberLogChannel.send(
+    `${member} has joined the server. they joined at ${
+      member.joinedAt
+    }. the server now has ${member.guild.memberCount} members.`
+  );
+});
+client.on('guildMemberRemove', member => {
+  const memberLogChannel = member.guild.channels.find('name', 'member-logs');
+  memberLogChannel.send(
+    `${member} has left the server. they joined at ${
+      member.joinedAt
+    }. the server now has ${member.guild.memberCount} members.`
+  );
+});
+
 client.on('message', async msg => {
+  const logChannel = msg.guild.channels.find('name', 'logs');
   if (msg.content.startsWith(prefix)) {
-    const args = msg.content.slice(prefix.length).split(/ +/);
+    const args = msg.content
+      .slice(prefix.length)
+      .trim()
+      .split(/ +/g);
     const command = args.shift().toLowerCase();
     console.log(`command: ${command}`);
     console.log(`args: ${args}`);
 
+    if (command === 'say') {
+      const sayMessage = args.join(' ');
+      msg.delete().catch(o3o => {});
+      msg.channel.send(sayMessage);
+      logChannel
+        .send(
+          `user ${msg.author.tag} sent the command \`${command} ${args.join(
+            ' '
+          )}\` in ${msg.channel}`
+        )
+        .catch(err => console.log(err));
+    }
     if (command === 'delete') {
       const deleteCount = args;
       if (!deleteCount || deleteCount < 2 || deleteCount > 100)
@@ -28,25 +61,78 @@ client.on('message', async msg => {
         );
       msg.channel
         .fetchMessages({ limit: deleteCount })
-        .then(msgs => msg.channel.bulkDelete(msgs));
+        .then(msgs => msg.channel.bulkDelete(msgs))
+        .then(msgs =>
+          logChannel.send(
+            `user ${msg.author.tag} sent the command \`${command} ${args.join(
+              ' '
+            )}\` in ${msg.channel}. this deleted ${msgs.size} messages.`
+          )
+        );
+    }
+    if (command === 'kick') {
+      if (
+        !msg.member.roles.some(r => ['big frog', 'Moderator'].includes(r.name))
+      )
+        return msg.reply(
+          `${
+            msg.author
+          } doesn't have the correct role to use the command: \`${command}.\``
+        );
+      let member = msg.mentions.members.first();
+      if (!member) return msg.reply('no user matches this name');
+      if (!member.kickable)
+        return msg.reply(
+          `bot does not have proper permissions to use the command: \`${command}.\``
+        );
+      let reason = args.slice(1).join(' ');
+      if (!reason)
+        return msg.reply(`\`${command}.\` requires a reason argument`);
+      await member
+        .kick(reason)
+        .catch(error => msg.reply(`couldn't kick because of: ${error}`));
+      logChannel.send(
+        `user ${msg.author.tag} sent the command \`${command} ${args.join(
+          ' '
+        )}\` in ${msg.channel}.`
+      );
+      logChannel.send(
+        `${member.user.tag} has been kicked by ${
+          msg.author.tag
+        } for the reason: \`${reason}\``
+      );
+    }
+    if (command === 'ban') {
+      if (!msg.member.roles.some(r => ['big frog'].includes(r.name)))
+        return msg.reply(
+          `${
+            msg.author
+          } doesn't have the correct role to use the command: \`${command}.\``
+        );
+      let member = msg.mentions.members.first();
+      if (!member) return msg.reply('no user matches this name');
+      if (!member.bannable)
+        return msg.reply(
+          `bot does not have proper permissions to use the command: \`${command}.\``
+        );
+      let reason = args.slice(1).join(' ');
+      if (!reason)
+        return msg.reply(`\`${command}.\` requires a reason argument`);
+      await member
+        .ban(reason)
+        .catch(error => msg.reply(`couldn't ban because of: ${error}`));
+      logChannel.send(
+        `user ${msg.author.tag} sent the command \`${command} ${args.join(
+          ' '
+        )}\` in ${msg.channel}.`
+      );
+      logChannel.send(
+        `${member.user.tag} has been kicked by ${
+          msg.author.tag
+        } for the reason: \`${reason}\``
+      );
     }
   }
 });
 
 client.login(token);
-
-// comment hell
-// msg
-//   .delete()
-//   .then(msg =>
-//     console.log(
-//       `deleted "${msg.content}" from ${msg.author} at ${msg.createdAt}. `
-//     )
-//   );
-// if (!deleteCount || deleteCount < 2 || deleteCount > 100)
-//   return msg.reply(
-//     'Please provide a number between 2 and 100 for the number of messages to delete'
-//   );
-// const fetched = await msg.channel.fetchMessages({
-//   count: deleteCount
-// });
