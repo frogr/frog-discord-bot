f46feee50374e94375f937e8e49e886cdb9165a5
frogr <austindanielfrench@gmail.com>
2018-09-03 22:37:59 -0700

    covert helped me find a bug!

--- commands/gay.js
+++ commands/gay.js
@@ -1,12 +1,13 @@
 const logOutput = require('../helpers/logOutput');
 
 const gay = (msg, command, args) => {
-  const counter = Math.floor(Math.random() * 3) + 1;
+  const counter = Math.floor(Math.random() * 4) + 1;
   const gayTweets = [
-    'https://twitter.com/frog_js/status/1029523590226440192',
+    null,
     'https://twitter.com/frog_js/status/1029141745844383745',
     'https://twitter.com/frog_js/status/1020479011317796865',
-    'https://twitter.com/frog_js/status/1005860494001860608'
+    'https://twitter.com/frog_js/status/1005860494001860608',
+    'https://twitter.com/frog_js/status/1029523590226440192'
   ];
   msg.channel.send(gayTweets[counter]);
   logOutput(msg, command, args);
